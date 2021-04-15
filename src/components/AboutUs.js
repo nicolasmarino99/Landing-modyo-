5e8488756a32d25326983c1d23cd22196f437bc5
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2021-04-14 22:57:13 -0500

    add image to component

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,8 +1,10 @@
+import happyWoman from '../../assets/images/about_1.jpg';
+
 const AboutUs = () => (
   `<div class="AboutUs">
             <h1>About Us</h1>
             <div class="info-container">
-                <img src="" alt="">
+                <img src="${happyWoman}" alt="happyWoman">
                 <div class="info">
                     <h2>Create interactive prototypes</h2>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing </p>
