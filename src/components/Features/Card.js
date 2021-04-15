a227073d6f662a54ff98c3d74c8a28de59786a5d
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2021-04-15 18:38:35 -0500

    add styles to Card

--- src/components/Features/Card.js
+++ src/components/Features/Card.js
@@ -4,7 +4,7 @@ const Card = (Iconclass, title) => (
             <span class="${Iconclass}"></span>
             <h3>${title}</h3>
             <p>Lorem ivsum dolor, sit amet consectetur adipisicing elit. Laboriosam adipisci corporis asperiores! Accusantium pariatur non deserunt ullam</p>
-            <a href="#">Learn More</a>
+            <a href="#" class="color-main">Learn More</a>
         </div>`
 );
 
