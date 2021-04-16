20297d76de4de636cd8ddb85427c723799d67f8d
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2021-04-15 19:51:10 -0500

    add testimonials styles

--- src/components/Testimonials/Slides.js
+++ src/components/Testimonials/Slides.js
@@ -1,7 +1,7 @@
 const Slides = (img, testimonial, name) => (
-  `<div class="icon-cards__item d-flex align-items-center justify-content-center">
+  `<div class="Slides icon-cards__item d-flex align-items-center justify-content-center">
         <img src="${img}" alt="Person img">
-        <p>${testimonial}</p>
+        <p>"${testimonial}"</p>
         <b>${name}</b>
     </div>`
 );
